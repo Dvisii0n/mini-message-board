3630c1480320ca5ab5ea465f4059827aaa37590a
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-01-26 18:11:01 -0600

    Moved middleware to controllers folder

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,47 +1,16 @@
 import { Router } from "express";
+import {
+	createMessage,
+	getForm,
+	getIndex,
+	getMessage,
+} from "../controllers/indexController.js";
 
 const indexRouter = Router();
 
-const title = "Mini message board";
+indexRouter.get("/", getIndex);
 
-const messages = [];
-
-function createMessage(req, res) {
-	try {
-		const messageText = req.body.messageText;
-		const userName = req.body.userName;
-		messages.push({
-			id: crypto.randomUUID(),
-			user: userName,
-			text: messageText,
-			added: new Date().toLocaleString(),
-		});
-		res.redirect("/");
-	} catch (error) {
-		res.status(500).send("Error 500 Internal server Error");
-	}
-}
-
-function getMessage(req, res, next) {
-	try {
-		const message = messages.filter((msg) => msg.id === req.params.id)[0];
-		if (!message) {
-			next();
-			return;
-		}
-		res.render("details", { title: title, message: message });
-	} catch (error) {
-		res.status(500).send("Error 500 Internal server Error");
-	}
-}
-
-indexRouter.get("/", (req, res) => {
-	res.render("index", { title: title, messages: messages });
-});
-
-indexRouter.get("/new", (req, res) => {
-	res.render("form", { title: title });
-});
+indexRouter.get("/new", getForm);
 
 indexRouter.post("/new", createMessage);
 
