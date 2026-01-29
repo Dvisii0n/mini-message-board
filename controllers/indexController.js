6297e09224987579919791c51621ef33020dae35
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-01-28 18:51:28 -0600

    Added input validation and sanitization, added data fetching via queries

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,25 +1,46 @@
 export const title = "Mini message board";
-export const messages = [];
+import db from "../db/queries.js";
+import { body, matchedData, validationResult } from "express-validator";
 
-export function createMessage(req, res) {
-	try {
-		const messageText = req.body.messageText;
-		const userName = req.body.userName;
-		messages.push({
-			id: crypto.randomUUID(),
-			user: userName,
-			text: messageText,
-			added: new Date().toLocaleString(),
-		});
-		res.redirect("/");
-	} catch (error) {
-		throw error;
-	}
-}
+const alphaErr = "characters must be alphanumeric";
+const userLengthErr = "Username length must be between 1 and 25 characters";
+const msgLengthErr = "Message length must be between 3 and 255 characters";
+const validateMessage = [
+	body("userName")
+		.trim()
+		.isAlphanumeric()
+		.withMessage(`Username ${alphaErr}`)
+		.isLength({ min: 1, max: 25 })
+		.withMessage(userLengthErr),
+	body("messageText")
+		.isAlpha("en-US", { ignore: " " })
+		.withMessage(`Message ${alphaErr}`)
+		.isLength({ min: 3, max: 255 })
+		.withMessage(msgLengthErr),
+];
 
-export function getMessage(req, res, next) {
+export const createMessage = [
+	validateMessage,
+	async (req, res) => {
+		try {
+			const errors = validationResult(req);
+			if (!errors.isEmpty()) {
+				res.render("form", { title: title, errors: errors.array() });
+				return;
+			}
+			const { userName, messageText } = matchedData(req);
+			await db.createMessage(userName, messageText);
+			res.redirect("/");
+		} catch (error) {
+			throw error;
+		}
+	},
+];
+
+export async function getMessage(req, res, next) {
 	try {
-		const message = messages.filter((msg) => msg.id === req.params.id)[0];
+		const data = await db.getMessage(req.params.id);
+		const message = data[0];
 		if (!message) {
 			next();
 			return;
@@ -30,8 +51,9 @@ export function getMessage(req, res, next) {
 	}
 }
 
-export function getIndex(req, res) {
+export async function getIndex(req, res) {
 	try {
+		const messages = await db.getAllMessages();
 		res.render("index", { title: title, messages: messages });
 	} catch (error) {
 		throw error;
